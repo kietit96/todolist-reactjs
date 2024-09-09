f6f5ceb5b4287f58ed102477757424bafab26b72
Anh Kiet <kiet.it.96@gmail.com>
2024-09-09 17:39:43 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const [titleName, setTitleName] = useState("")
   const [todolist, setTodoList] = useState([])
   const [id, setId] = useState(0)
+
   const changeTextInput = useCallback((e) => {
     setTitleName(e.target.value)
   }, []);
 
   const AddToDoList = useCallback((e) => {
     setId(id + 1)
-    const isComplete = false
-    setTodoList([...todolist, { id: id, title: titleName, state: isComplete }])
+    setTodoList([...todolist, { id: id, title: titleName, state: false, filtered: false }])
     setTitleName("")
   }, [titleName, todolist, id]);
 
@@ -26,11 +26,42 @@ function App() {
 
   }, [todolist]);
 
+  const btnFilter = useCallback((filterType) => {
+    const newTodoList = [...todolist]
+    if (filterType === "all") {
+      newTodoList.map((val) => val.filtered = false
+      )
+    }
+    else if (filterType === "completed") {
+      newTodoList.map((val) => {
+        if (val.state === false) {
+          return val.filtered = true
+        } 
+        return val.filtered = false
+      })
+    }
+    else {
+      newTodoList.map((val) => {
+        if (val.state === true) {
+          return val.filtered = true
+        }
+        return val.filtered = false
+      })
+    }
+    setTodoList(newTodoList)
+  }, [todolist])
+  useEffect(() => {
+  }, [todolist])
   return (
     <div className="container">
       <div className="formTodo">
         <input value={titleName} name="titleName" placeholder='Tiêu đề' onChange={changeTextInput}></input>
         <button onClick={AddToDoList}>Thêm</button>
+      </div>
+      <div className="filter-box">
+        <button onClick={() => btnFilter("all")}>show all</button>
+        <button onClick={() => btnFilter("completed")}>show completed</button>
+        <button onClick={() => btnFilter("not-completed")}>show not completeted</button>
       </div>
       <ul className='list-todo'>
         <TodoList onTodoClick={handleClick} todoList={todolist}></TodoList>
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,7 +1,7 @@
 
 function Todo({ todo, onTodoClick }) {
     return (
-        <li className={todo.state ? "complete" : ""}><span>{todo.title}</span> <button type="button" onClick={() => onTodoClick(todo.id)}>v</button></li>
+        <li className={[todo.filtered ? "filtered" : "", todo.state ? "complete" : ""].join(" ")}><span>{todo.title}</span> <button type="button" onClick={() => onTodoClick(todo.id)}>v</button></li>
     )
 }
 export default Todo
